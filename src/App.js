83f9b6b5e858084d52252c1b55a2f42653c37045
loressl <loryssl@hotmail.com>
2020-12-26 00:35:50 -0300

    Simple Calculator Design.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Calculator from './main/Calculator';
+
+function App() {
+  return (
+    <div>
+      <h1>Calculadora</h1>
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Button.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Button.css';
+
+function Button(props) {
+    let classes = 'button ';
+    classes += props.operation ? 'operation' : '';
+    classes += props.double ? 'double' : '';
+    classes += props.triple ? 'triple' : '';
+
+    return (
+        <button
+            onClick={_ => props.click && props.click(props.label)}
+            className={classes}>
+            {props.label}
+        </button>
+    )
+}
+
+export default Button;
+
